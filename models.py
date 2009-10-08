d4b574e55a8dd466ce71a2d546a10fba8e9b3972
Scott <scott@nlinfit.(none)>
2009-10-08 10:14:43 -0400

    bf

--- models.py
+++ models.py
@@ -440,7 +440,6 @@ class AutoCollection(Gallery):
             self._numfield = numfield
         def get_query_set(self, user=None):
             queryfield = self._parent.queryfield
-            try:
             if queryfield == 'date_taken' and (queryfield not in self._manager.model._meta.get_all_field_names()):
                 queryfield = 'date_beginning'
             return self._parent._getquery(self._manager.get_query_set(), queryfield, user=user, number=getattr(self._parent, self._numfield))
